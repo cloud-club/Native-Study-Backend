fe1e9aada2552553e759fdd4643ad7038fcaa558
jungmir <ham4051@gmail.com>
2022-04-08 23:52:25 +0900

    UPDATE upload music api feature

--- app/routers/music.py
+++ app/routers/music.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter, Form, Path, Query, UploadFile, status
+from datetime import datetime
+from uuid import uuid4
+
+from fastapi import APIRouter, File, Form, Path, Query, UploadFile, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from app.schema.response import music as music_resp
+from app.schema.response.common import RequestMetadata, ResponseMetadata
+from app.utils.time import elapsed, time2str
 
 router = APIRouter()
 
@@ -21,9 +26,36 @@ async def upload_music(
     music_name: str = Form(...),
     singer_name: str = Form(...),
     release_date: str = Form(None),
-    music_file: UploadFile = Form(...),
+    music_file: UploadFile = File(...),
 ) -> JSONResponse:
     """음악 파일과 음악 메타정보를 저장 합니다."""
+    request_time = datetime.now()
+    request_metadata = RequestMetadata(
+        request_id=str(uuid4()),
+        request_data={
+            "music_id": 112453,
+            "music_name": music_name,
+            "singer_name": singer_name,
+            "release_date": release_date,
+            "play_total": 128,
+            "music_file": music_file.filename,
+        },
+    )
+
+    # @TODO: file validation
+    # @TODO: save file in minIO
+    # @TODO: save data in DB
+
+    response_time = datetime.now()
+    response_metadata = ResponseMetadata(
+        request_time=time2str(request_time),
+        response_time=time2str(response_time),
+        elapsed_time=elapsed(request_time, response_time),
+    )
+    response = music_resp.UploadMusicResponse(
+        request_metadata=request_metadata,
+        response_metadata=response_metadata,
+    )
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
         content=jsonable_encoder(response),
