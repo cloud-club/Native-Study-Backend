be582613165fcf95a137468d167d2aa245097572
jungmir <ham4051@gmail.com>
2022-04-12 22:37:07 +0900

    FIX undo previous version

--- app/utils/file.py
+++ app/utils/file.py
@@ -1,29 +1,27 @@
+from io import BytesIO
+
 import mutagen
 from starlette.datastructures import UploadFile
 
 from app.common.type import Audio, t
+from app.database.storage import storage
 
 
 class AudioFile:
     _file: Audio
+    binary: UploadFile
     metadata: t.Dict
     content_type: str
+    size: int
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size: int):
-        self.__size = size
-
-    def __init__(self, file: UploadFile):
-        content_type = file.content_type
+    def __init__(self, file: bytes, content_type: str):
         if not content_type.startswith("audio"):
             ext = content_type.split("/")[-1]
             raise ValueError(f"${ext} is not supported type")
+        self.size = len(file)
+        self.binary = BytesIO(file)
         self.content_type = content_type
-        self._file = mutagen.File(file.file)
+        self._file = mutagen.File(BytesIO(file))
         self.metadata = self.parse_metadata()
 
     def parse_metadata(self):
@@ -39,22 +37,22 @@ class AudioFile:
         length = metadata.length
         sample_rate = metadata.sample_rate
         channels = metadata.channels
-        return dict(
-            length=length,
-            rate=sample_rate,
-            channels=channels,
-        )
+        return dict(length=length, rate=sample_rate, channels=channels, size=self.size,)
 
     def _parse_wav_metadata(self) -> t.Dict:
         metadata = self._file.info
         length = metadata.length
         sample_rate = metadata.sample_rate
         channels = metadata.channels
-        return dict(
-            length=length,
-            rate=sample_rate,
-            channels=channels,
-        )
+        return dict(length=length, rate=sample_rate, channels=channels, size=self.size,)
 
-    def save(self):
-        pass
+    async def save(self, filename: str, bucket_name: str = "music") -> str:
+        result = storage.save(
+            bucket_name=bucket_name,
+            object_name=filename,
+            binary=self.binary,
+            length=self.size,
+        )
+        if not result:
+            raise ValueError
+        return f"/{bucket_name}/{filename}"
