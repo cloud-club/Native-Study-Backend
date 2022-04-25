d8cf62be5bd3b383607dbda361ec6d86fd73eb21
jungmir <ham4051@gmail.com>
2022-04-25 21:59:47 +0900

    Test argoCD non-stop deploy

--- app/routers/index.py
+++ app/routers/index.py
@@ -7,11 +7,18 @@ router = APIRouter()
 @router.get(
     "/",
     responses={
-        200: {
-            "content": {"text": "pong"},
-            "description": "check server status",
-        }
+        200: {"content": {"text": "pong"}, "description": "check server status",}
     },
 )
 def ping():
     return PlainTextResponse("pong")
+
+
+@router.get(
+    "/test",
+    responses={
+        200: {"content": {"text": "test"}, "description": "check server status",}
+    },
+)
+def test_api():
+    return PlainTextResponse("test api service")
